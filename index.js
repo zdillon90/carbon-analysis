fe3b2d2acee9189fa487434824775cf3e78e35b9
Zach Dillon <zach@shapeways.com>
2019-06-12 09:56:52 -0400

    stl check
    Added a check to make sure that the stl file was there before uploading. Also added the machine time to the output file. Added some more sleeps for a slower connection.

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ async function carbonGo(model, resinType) {
   const resinProject = resinSelect(resinType);
   const automationProjectURL = `http://m2328.shapeways.print.carbon3d.com/projects/${resinProject}`;
   const carbonURL = 'https://shapeways.print.carbon3d.com/printers';
+  // const stlPath = `C:\\Users\\Zach Dillon\\Documents\\Carbon\\analysis_models\\${model}`;
+  const stlPath = `C:\\Users\\Zach Dillon\\Downloads\\${model}`;
   await gotoURL(page, carbonURL);
   await login(page);
   const printPage = await newTab(browser);
@@ -53,15 +55,21 @@ async function carbonGo(model, resinType) {
   if (url !== automationProjectURL) {
     await login(printPage);
   }
-  await uploadModel(
-    printPage,
-    `C:\\Users\\Zach Dillon\\Downloads\\${model}`,
-    model
-  );
-  // TODO place place the path of the model upload in the .env file
+  console.log('Checking to model path');
+  const fileExists = fs.access(stlPath, fs.F_OK, err => {
+    if (err) {
+      console.error(err);
+      return 'err';
+    }
+    console.log('file exists');
+    return 'file';
+  });
+  console.log('fileExists:', fileExists);
+  await uploadModel(printPage, stlPath, model);
+  console.log('Upload should be complete');
   await deleteOldModel(printPage);
   await minFootprint(printPage);
-  await maxFootPrint(printPage);
+  // await maxFootPrint(printPage);
   await checkFit(printPage);
   await layoutPart(printPage);
   await supportPart(printPage);
@@ -71,7 +79,7 @@ async function carbonGo(model, resinType) {
 }
 
 async function main() {
-  const rawData = fs.readFileSync('stls.json');
+  const rawData = fs.readFileSync('stls1.json');
   const data = JSON.parse(rawData);
   const outputList = [];
   for await (const stlFile of data) {
@@ -79,17 +87,21 @@ async function main() {
     // await convertModel(model, version);
     const stlFileName = `${model}.stl`;
     console.log(stlFileName);
-    const rawCost = await carbonGo(stlFileName, resin);
+    const rawValues = await carbonGo(stlFileName, resin);
+    const rawCost = rawValues.cost;
+    const { machineTimeMin } = rawValues;
+    // TODO if the model is not found then continue the loop
     const cost = `$${rawCost.toFixed(2)}`;
     const modelWithCost = {
       model,
       cost,
+      machineTimeMin,
       resin,
     };
     outputList.push(modelWithCost);
+    const outputData = JSON.stringify(outputList, null, 2);
+    fs.writeFileSync('output.json', outputData);
   }
-  const outputData = JSON.stringify(outputList, null, 2);
-  fs.writeFileSync('output.json', outputData);
 }
 
 main();
